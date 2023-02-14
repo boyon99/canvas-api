a20078e7a877e587e585fb55fe957d567d0eec73
boyon99 <bo_yon_99@naver.com>
2023-02-14 16:30:52 +0900

    :memo: feat: Add fill color
    색 채우기 기능 추가

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ canvas.height = 500;
 const lineWidth = document.getElementById("line-width")
 ctx.lineWidth = lineWidth.value
 
-function onLineWidthChange(e){
+function onLineWidthChange(e) {
   ctx.lineWidth = e.target.value
 }
 
@@ -19,44 +19,67 @@ lineWidth.addEventListener("change", onLineWidthChange)
 const color = document.getElementById("color")
 color.addEventListener("change", onColorChange)
 
-function onColorChange(e){
+function onColorChange(e) {
   ctx.strokeStyle = e.target.value
   ctx.fillStyle = e.target.value
 }
 
 // color options
 const colorOptions = Array.from(document.getElementsByClassName('color-option')) // document는 배열이 아니므로 배열로 변경
-colorOptions.forEach(color=>color.addEventListener('click',onColorClick))
+colorOptions.forEach(color => color.addEventListener('click', onColorClick))
 
-function onColorClick(e){
+function onColorClick(e) {
   ctx.strokeStyle = e.target.dataset.color
   ctx.fillStyle = e.target.dataset.color
   color.value = e.target.dataset.color
 }
 
+// fill & draw mode
+const modeBtn = document.getElementById("mode-btn")
+let isFilling = false
+
+modeBtn.addEventListener('click', onModeClick)
+
+function onModeClick(e) {
+  if (isFilling) {
+    isFilling = false
+    modeBtn.innerText = "채우기"
+  } else {
+    isFilling = true
+    modeBtn.innerText = "그리기"
+  }
+}
+
 // palette
 let isPainting = false
 
-function onMove(e){
-  if(isPainting){
-    ctx.lineTo(e.offsetX,e.offsetY)
+function onMove(e) {
+  if (isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY)
     ctx.stroke()
     return;
   }
   ctx.beginPath()
-  ctx.moveTo(e.offsetX,e.offsetY)
+  ctx.moveTo(e.offsetX, e.offsetY)
 
 }
 
-function startPainting(e){
+function startPainting(e) {
   isPainting = true;
 }
 
-function cancelPainting(e){
+function cancelPainting(e) {
   isPainting = false;
+}
+
+function onCanvasClick(e){
+  if(isFilling){
+    ctx.fillRect(0,0,500,500)
+  }
 }
 
 canvas.addEventListener('mousemove', onMove)
 canvas.addEventListener('mousedown', startPainting)
 canvas.addEventListener('mouseup', cancelPainting)
 canvas.addEventListener('mouseleave', cancelPainting)
+canvas.addEventListener('click', onCanvasClick)

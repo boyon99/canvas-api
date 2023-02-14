ff08702cf74f5ec89c815f585a53468b7c60b424
boyon99 <bo_yon_99@naver.com>
2023-02-14 16:06:57 +0900

    :memo: feat: Add line width
    그림판에 선두께 조절 기능 추가

--- app.js
+++ app.js
@@ -4,15 +4,29 @@ const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용
 canvas.width = 500;
 canvas.height = 500;
 
-ctx.lineWidth = 2
 let isPainting = false
 
+// line-width
+const lineWidth = document.getElementById("line-width")
+ctx.lineWidth = lineWidth.value
+
+function onLineWidthChange(e){
+  ctx.lineWidth = e.target.value
+
+}
+
+lineWidth.addEventListener("change", onLineWidthChange)
+
+
+
+// mouse move
 function onMove(e){
   if(isPainting){
     ctx.lineTo(e.offsetX,e.offsetY)
     ctx.stroke()
     return;
   }
+  ctx.beginPath()
   ctx.moveTo(e.offsetX,e.offsetY)
 
 }
